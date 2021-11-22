6d66e2737ef2ad78d4db083e29fcd32d4d585f80
a-pineau <adrian.pineau@gmail.com>
2021-11-22 23:55:37 +0100

    display board method

--- Board.py
+++ Board.py
@@ -7,8 +7,6 @@ import Marble as M
 
 
 class Board():
-    ORIENTATIONS = ["N", "S", "E", "W"]
-
     def __init__(self):
         """
         TODO
@@ -17,6 +15,7 @@ class Board():
         self.nb_white = self.nb_black = 14
 
         # We assume the standard initial configuration commonly used
+        # So its hard-coded
         self.board = [[2, 2, 2, 2, 2, 0, 0, 0, 0],
                       [2, 2, 2, 2, 2, 2, 0, 0, 0],
                       [1, 1, 2, 2, 2, 1, 1, 0, 0],
@@ -38,20 +37,22 @@ class Board():
         # 2: w = white marble
         # 3: b = black marble
         num_char = {"0": "f", "1": "e", "2": "w", "3": "b"}
-        current_board = "\n"
+        current_board = "  0 1 2 3 4 5 6 7 8    |         BOARD\n"
         mid_point = math.floor(self.dimension / 2)
 
+        extra_space = 4 # For visibiliy purposes
         j, k = 1, mid_point
         for i in range(self.dimension):
-            str_row = (num_char[str(e)] for e in self.board[i] if e != 0)
+            str_row = list(str(e) if e != 0 else " " for e in self.board[i])
+            current_board += f"{i} " + " ".join(str_row) + extra_space * " " + "|   "
             if i < mid_point:
-                current_board += k * " " + "".join(str_row) + "\n"
+                current_board += k * " " + " ".join(str_row) + "\n"
                 k -= 1
             elif i > mid_point:
-                current_board += "".join(str_row) + j * " " + "\n"
+                current_board += j * " " + " ".join(str_row).lstrip() + "\n"
                 j += 1
             else:
-                current_board += "".join(str_row) + "\n"
+                current_board += " ".join(str_row) + "\n"
 
         return current_board
 
@@ -70,23 +71,25 @@ class Board():
         """
         while True:
             expr = r'^[0-9]{2}$'
-            move = input(
-                'Pick a location on the board (09): ')
-            orentation = input("Pick an orientation (N, S, E, W)")
-
-            if orientation not in ORIENTATIONS:  # check if the orientation is correct
-                print("Invalid orientation!")
-                continue
+            move = input('Pick a location on the board (08): ')
             if re.match(expr, move) is None:  # check if the move is correct
                 print('Invalid move!')
                 continue
 
             row, col = move
-            current_move = self.board[row][col]
+            current_move = self.board[int(row)][int(col)]
             # check if the player can play here
             if current_move == 0 or current_move == 1 or current_move != marble_type:
-                print("You canno\'t play here!")
+                print("current move=", current_move, sep="")
+                print("You cannot play here!")
                 continue
+
+            orientation = input("Pick an orientation (N, S, E, W): ")
+            if orientation.upper() not in ["N", "S", "E", "W"]: # check if the orientation is correct
+                print("Invalid orientation!")
+                continue
+
+            print(row, col, orientation)
 
     def move_marble(self):
         """
@@ -96,6 +99,5 @@ class Board():
 
 if __name__ == "__main__":
     B = Board()
-    B.ask_move("a")
     print(B)
-    B.count_marble()
+    # B.ask_move("a")
